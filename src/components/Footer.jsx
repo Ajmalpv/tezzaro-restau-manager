4203a97999ba02441afeb4306e20a0900ecf4ef4
Ajmal_pv <ajmaljbaju@gmail.com>
2025-10-29 23:14:36 +0530

    footer change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <Box sx={{ bgcolor: 'primary.main', color: 'white', py: 2, textAlign: 'center', mt: 3 }}>
       <Typography variant="body2">
-        © 2025 TEZZARO Restaurant | Created with ❤️ by Brandmefy
+        © 2025 TEZZARO Restaurant | Cooked with ❤️ by Brandmefy
       </Typography>
     </Box>
   );
